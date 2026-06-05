440e56a4c76d2e5341490465c2a64b0961f8b3de
jose <joseruizperez635@gamil.com>
2026-06-04 23:28:34 -0500

    Implementacion de santucm y mejorado

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // controlador que nos permitira autenticar los usuarios y registrarlos
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Credenciales incorrectas'], 401);
+        }
+
+        //eliminamos unos tokens anteriores del usuario autenticado esto con el fin de que no se dupliquen o se alamcenen tokens anteriores
+        $user->tokens()->delete();
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json(['message' => 'Login exitoso', 'token' => $token, 'user' => $user, 'mensaje' => 'Bienvenido: '.$user->name], 200);
+    }
+
+    public function register(Request $request){
+        // aqui registramos un nuevo usuario
+         $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+        $token = $user->createToken('api-token')->plainTextToken;
+       return response([
+           'message' => 'Usuario creado correctamente',
+           'user' => $user->name.' con sus datos de gmail '.$user->email,
+           'token' => $token
+       ]);
+        
+    }
+
+    public function logout(Request $request){
+        // aqui cerramos la sesion del usuario y eliminamos el token del usuario que cerro la sesion esto con el fin de que no puedan hacer consultas despues de cerrar la sesion
+
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logout exitoso'], 200);
+        
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,24 @@ use  App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\funcionCargoController;
 use App\Http\Controllers\CargoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-    // aqui implementamos laravel sanctum
+
+    Route::POST('/login', [AuthController::class, 'login']);
+    Route::POST('/register', [AuthController::class, 'register']);
+    
+    // aqui implementamos laravel sanctum y aseguramos las rutas para los usuarios no autenticados
+
+    Route::middleware('auth:sanctum')->group(function () {
+    Route::POST('/logout', [AuthController::class, 'logout']);    
     Route::apiResource('cargos', CargoController::class);
     Route::apiResource('empleados', EmpleadoController::class);
     Route::apiResource('funcionCargos', funcionCargoController::class);
+    });
+
+
 
