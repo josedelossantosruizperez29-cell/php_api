fa2e284e750ac050c3b044a24119c30cc1a10611
jose <joseruizperez635@gamil.com>
2026-06-05 01:16:42 -0500

    implementacion de test para el crud de empleados

---
+++ tests/Feature/test_eliminar_empleadoTest.php
@@ -0,0 +1,25 @@
+<?php
+// el usuario puede eliminar un empleado solo si esta autenticado
+namespace Tests\Feature;
+use Tests\TestCase;
+use App\Models\Cargo;
+use App\Models\Empleados;
+use Laravel\Sanctum\Sanctum;
+use Database\Factories\EmpleadosFactory;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class eliminar_empleado extends TestCase{
+    use RefreshDatabase;
+
+    public function test_puede_eliminar_un_empleado_solo_si_el_usuario_esta_autenticado(): void{
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+        $cargo = Cargo::factory()->create();
+       $empleado = Empleados::factory()->create([
+            'id_cargo' => $cargo->id
+        ]);
+        $response=$this->deleteJson("api/empleados/{$empleado->id}");
+        $this->assertDatabaseMissing('empleados', ['id' => $empleado->id]);
+        $response->assertStatus(200);
+}
+}
